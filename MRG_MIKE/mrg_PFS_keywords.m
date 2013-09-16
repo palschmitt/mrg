0ef6818f4f1e167c3ad64ba89dd908e64838a571
Daniel Pritchard <daniel@pritchard.co>
2013-09-16 16:17:18 +0100

    Major do-over of mrg_create_npp_langdef.m

---
+++ MRG_MIKE/mrg_PFS_keywords.m
@@ -0,0 +1,78 @@
+function keywords = mrg_PFS_keywords
+% Scans a directory, reads all the ecolab, m21fm and plc files therein and
+% returns the [header] keywords
+%
+% INPUT
+%   None. The user is prompted to select a directory which can be the DHI
+%   installation directory or any folder with a whole heap of PFS formatted
+%   files from which to extract the keywords.  
+%
+% OUTPUT
+%   keywords    A MATLAB structure with fields:
+%                   variable_len: PFS keywords which might be variable in
+%                   length (e.g. STATE_VARIABLE_X; where X is an integer)
+%                   fixed_len: PFS keywords which are probably not variable
+%                   in length (e.g. ECO_LAB_SETUP)
+%
+% NOTES
+%   This can be called directly, if needed, but it is designed to work
+%   alongside mrg_create_npp_langdef.m
+%
+% AUTHORS
+%   Daniel Pritchard
+%
+% LICENCE
+%   Code distributed as part of the MRG toolbox from the Marine Research
+%   Group at Queens Univeristy Belfast (QUB) School of Planning
+%   Architecture and Civil Engineering (SPACE). Distributed under a
+%   creative commons CC BY-SA licence, retaining full copyright of the
+%   original authors.
+%
+%   http://creativecommons.org/licenses/by-sa/3.0/
+%   http://www.qub.ac.uk/space/
+%   http://www.qub.ac.uk/research-centres/eerc/
+%
+% DEVELOPMENT
+%   v 1.0   2013-09-16
+%           First version. DP
+%
+% TODO
+%   Allow variable extensions at run time.
+%
+%% Function Begin!
+folder_name = uigetdir('C:/', 'Please select the DHI installation directory');
+fileList = mrg_oswalk(folder_name);
+
+x1 = regexp(fileList, '.*\.ecolab$', 'match');
+y1 = x1(~cellfun('isempty',x1));
+
+x2 = regexp(fileList, '.*\.m21fm$', 'match');
+y2 = x2(~cellfun('isempty',x2));
+
+x3 = regexp(fileList, '.*\.plc$', 'match');
+y3 = x3(~cellfun('isempty',x3));
+
+a1 = [y1;y2;y3];
+
+allout = {};
+for a = 1:length(a1)
+    text = fileread(char(a1{a}));
+    matches = regexp(text, '\[(\w+)\]\s+', 'tokens');
+    allout = [allout;matches.'];
+end
+
+varlen = regexp([allout{:}].', '(\w+_)\d+', 'tokens');
+index = ~cellfun('isempty',varlen);
+
+varlen = varlen(index);
+varlen = [varlen{:}];
+
+notvarlen = allout(~index);
+
+varlenout = unique([varlen{:}]);
+notvarlenout = unique([notvarlen{:}]);
+
+keywords = struct();
+keywords.variable_len = varlenout;
+keywords.fixed_len = notvarlenout;
+end
--- MRG_MIKE/mrg_create_npp_langdef.m
+++ MRG_MIKE/mrg_create_npp_langdef.m
@@ -1,13 +1,52 @@
 function mrg_create_npp_langdef()
-% Creates a language definition file for NotePad++ from some template files.
-% 
-% Requires userDefineLang_PFS_Template.xml, pfs_static_list.txt and 
-% pfs_variable_list.txt which can be found in Dans MIKE PFS respository:
-% https://github.com/dpritchard/mike_pfs_npp
-% 
-% All that being said, you may as well just get the generated XML from the
-% above link!
-
+% Creates a language definition file for NotePad++ by scanning the DHI
+% installation directory and extracting the information from the files it
+% finds there.
+%
+% INPUT
+%   None. The user is prompted to select the
+%   userDefineLang_PFS_Template.xml file.  
+%
+% OUTPUT
+%   NO OUTPUT AT CONSOLE.
+%   Three files are generated:
+%       userDefineLang.xml: The language definition file
+%       pfs_static_list.txt: A list of 'static' PFS keywords
+%       pfs_variable_list.txt: The 'variable' PFS keywords
+%
+% NOTES
+%   The three files will be written to the same folder as the
+%   userDefineLang_PFS_Template.xml file, and will overwirte any files with
+%   the same name.  
+%
+% REQUIREMENTS
+%   Requires userDefineLang_PFS_Template.xml which can be found in Dans 
+%   MIKE PFS respository: https://github.com/dpritchard/mike_pfs_npp
+%   
+%   All that being said, you may as well just get the generated XML from 
+%   the above link!
+%
+% AUTHORS
+%   Daniel Pritchard
+%
+% LICENCE
+%   Code distributed as part of the MRG toolbox from the Marine Research
+%   Group at Queens Univeristy Belfast (QUB) School of Planning
+%   Architecture and Civil Engineering (SPACE). Distributed under a
+%   creative commons CC BY-SA licence, retaining full copyright of the
+%   original authors.
+%
+%   http://creativecommons.org/licenses/by-sa/3.0/
+%   http://www.qub.ac.uk/space/
+%   http://www.qub.ac.uk/research-centres/eerc/
+%
+% DEVELOPMENT
+%   v 1.0   2013-03-??
+%           First version. DP
+%   v 1.1   2013-09-16
+%           Updated across the board and refactored to use mrg_oswalk() 
+%           and mrg_PFS_keywords()  
+%
 %% Get the Template
 % We assume the files with the lists are in same directory
 old_dir = cd;
@@ -18,78 +57,34 @@ s = textscan(fileID,'%s','Delimiter','\n');
 fclose(fileID);
 s = s{1};
 
-%% Static Items
-fileID = fopen('pfs_static_list.txt');
-static_items = textscan(fileID,'%s', 'Delimiter', '\n');
-fclose(fileID);
-
-loc_static = 1;
-trim_static = 0;
-static_items_start_out = cell(1,length(static_items{1}));
-static_items_keyword_out = cell(1,length(static_items{1}));
-for i = 1:length(static_items{1})
-    title_match = regexp(static_items{1}(i),'^<--\s*[\w]+\s*-->');
-    if title_match{1}
-        disp(static_items{1}(i))
-        trim_static = trim_static+1;
-    else
-        static_items_start_out(loc_static)=(strcat('[',static_items{1}(i), ']'));
-        static_items_keyword_out(loc_static)=static_items{1}(i);
-        loc_static = loc_static+1;
-    end
-    
-end
-static_items_start_out = static_items_start_out(1:end-trim_static);
-static_items_keyword_out = static_items_keyword_out(1:end-trim_static);
-
-%% Variable Items
-%[file, path] = uigetfile('.txt', 'Get Variable List');
-fileID = fopen('pfs_variable_list.txt');
-var_items = textscan(fileID,'%s', 'Delimiter', '\n');
-fclose(fileID);
-
-var_len = 50;
-
-loc_var = 1;
-trim_var = 0;
-var_items_start_out = cell(1,length(var_items{1})*var_len);
-var_items_keyword_out = cell(1,length(var_items{1})*var_len);
-for i = 1:length(var_items{1})
-    title_match = regexp(var_items{1}(i),'^<--\s*[\w]+\s*-->');
-    if title_match{1}
-        disp(var_items{1}(i))
-        trim_var = trim_var+1;
-    else
-        for j = 1:var_len
-            var_items_start_out(loc_var) = strcat('[',var_items{1}(i), '_', num2str(j), ']');
-            var_items_keyword_out(loc_var) = strcat(var_items{1}(i), '_', num2str(j));
-            loc_var = loc_var+1;
-        end
-    end
-end
-% Trim to account for headers...
-var_items_start_out = var_items_start_out(1:end-trim_var*var_len);
-var_items_keyword_out = var_items_keyword_out(1:end-trim_var*var_len);
+%% Get the header keywords
+keywords = mrg_PFS_keywords;
+keystring = [keywords.fixed_len, keywords.variable_len];
 
 %% Combine Items
-start_out = [static_items_start_out,var_items_start_out];
-keyword_out = [static_items_keyword_out,var_items_keyword_out];
-start_string = [sprintf('%s ',start_out{1:end-1}),start_out{end}];
-keyword_string=[sprintf('%s ',keyword_out{1:end-1}),keyword_out{end}];
+keyword_string = sprintf('%s ',keystring{:});
 
 %% Populate the template
-s = strrep(s,'[MRG_START_FILES]', start_string);
 s = strrep(s,'[MRG_KEYWORDS]', keyword_string);
 
 %% Write File
 [nrows,~]= size(s);
 filename = 'userDefineLang.xml';
 fid = fopen(filename, 'w');
-
 for row=1:nrows
-    fprintf(fid, '%s\n', s{row,:});
+    fprintf(fid, '%s\r\n', s{row,:});
 end
-
 fclose(fid);
+
+filename = 'pfs_static_list.txt';
+fid = fopen(filename, 'w');
+fprintf(fid, '%s\r\n', keywords.fixed_len{:});
+fclose(fid);
+
+filename = 'pfs_variable_list.txt';
+fid = fopen(filename, 'w');
+fprintf(fid, '%s\r\n', keywords.variable_len{:});
+fclose(fid);
+
 cd(old_dir);
 end
